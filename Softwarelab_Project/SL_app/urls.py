202ce78a74c2f315e9741f196040dbac7529d63a
Madhav Kotecha <50106950+madhavkotecha@users.noreply.github.com>
2024-10-15 14:44:41 +0530

    changed name of project to CuriosityBytes

--- Softwarelab_Project/SL_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('education_shorts/', views.get_education_shorts, name='get_education_shorts'),
-]
