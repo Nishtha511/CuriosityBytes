15c3dcdf8eff06cab2af0c780309f8cd9191d1ea
Madhav Kotecha <madhavkotecha99@gmail.com>
2024-10-18 14:13:44 +0530

    Google OAuth2 Authentication updated

--- CuriosityBytes/dashboard/views.py
+++ CuriosityBytes/dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -6,10 +6,16 @@ from django.http import JsonResponse
 # from .youtube_api import fetch_education_shorts
 
 def home(request):
-    return render(request, 'home.html')
+    if not request.user.is_authenticated:
+        return redirect('login')
+    return render(request, 'dashboard.html')
 
 def get_education_shorts(request):
     # subject = request.GET.get('subject', 'science')
     # videos = fetch_education_shorts(subject)  # Call the function from youtube_api.py
     # return JsonResponse(videos, safe=False)
     pass
+
+def is_loggedin(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
