306ee0d9ccb1fa2eba2d8b4e41a9ea435f21f4f1
Nishtha Vaghela <82824251+Nishtha511@users.noreply.github.com>
2024-10-20 23:59:57 +0530

    Update urls.py

--- CuriosityBytes/dashboard/urls.py
+++ CuriosityBytes/dashboard/urls.py
@@ -6,7 +6,7 @@ from .youtube_api import get_educational_shorts
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('education_shorts/', youtube_api.get_educational_shorts, name='get_education_shorts'),
+    path('get_educational_shorts/', get_educational_shorts, name='get_educational_shorts'),
     path('watch-history/', views.watch_history, name='watch_history'),
     path('search-history/', views.search_history, name='search_history'),
 
