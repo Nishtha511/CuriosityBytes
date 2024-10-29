011f4d3397a5dc9539aa16a0c08aa8b2ee35e7f7
Madhav Kotecha <madhavkotecha99@gmail.com>
2024-10-29 23:26:23 +0530

    fix

--- CuriosityBytes/CuriosityBytes/deployment.py
+++ CuriosityBytes/CuriosityBytes/deployment.py
@@ -22,25 +22,26 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 # STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-CONNECTION_STRING = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-params = {x.split('=')[0]:x.split('=')[1] for x in CONNECTION_STRING.split(' ')}
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': params['dbname'],
-        'HOST': params['host'],
-        'USER': params['user'],
-        'PASSWORD': params['password'],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# CONNECTION_STRING = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+# params = {x.split('=')[0]:x.split('=')[1] for x in CONNECTION_STRING.split(' ')}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': params['dbname'],
+#         'HOST': params['host'],
+#         'USER': params['user'],
+#         'PASSWORD': params['password'],
+#     }
+# }
 
 # STORAGES = {
 #     'default': {
